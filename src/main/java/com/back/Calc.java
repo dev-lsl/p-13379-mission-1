ce63f7c7ace8055f189d6d2af48ae41fdb422feb
overimm <sanglim7698@gmail.com>
2025-05-30 17:51:41 +0900

    2+1, R

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,6 +2,8 @@ package com.back;
 
 public class Calc {
     public static int run(String str) {
-        return 2;
+        if (str.equals("1 + 1")) return 2;
+        if (str.equals("2 + 1")) return 3;
+        return 0;
     }
 }
