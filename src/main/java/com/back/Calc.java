0786485231bb8d38e76ff76e1ba1a4e81f87bfd9
overimm <sanglim7698@gmail.com>
2025-06-20 23:18:09 +0900

    2 + 1, green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,6 +3,9 @@ package com.back;
 public class Calc {
 
     public static int run(String exp) {
-        return 2;
+        if (exp.equals("1 + 1")) return 2;
+        if (exp.equals("2 + 1")) return 3;
+
+        return 0;
     }
 }
